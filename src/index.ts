2ed0e35394988445002cf73b452d8c0446657198
Lorna Ryan <lornaniriain@gmail.com>
2020-05-26 19:07:04 +0100

    initial commit with test story

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+function scealAScriobh() {
+  let sceal = 'An samhradh seo caite, bhí mé ar mo laethanta saoire. Bhí an grian ag scoilteadh na gcloch. Bhí mé cois farraige. Go tobann, thosaigh mé ag troid le mo chara agus thosaigh mo shrón ag cur fola. Chuir an bhean glaoch ar mo thuismitheoirí. Ní dhéanfaidh mé dearmad go deo ar an lá sin. ';
+  
+  return sceal;
+}
+
+
+document.querySelector('main').innerHTML = scealAScriobh();
