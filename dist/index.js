43baa0432aa52a25841c945ee4ec2b7357712bd6
kieranwhooley <kieranwhooley@gmail.com>
2020-05-28 20:44:35 +0100

    Added button to generate

--- dist/index.js
+++ dist/index.js
@@ -5,17 +5,23 @@ function scéalAScríobh() {
     var scéal = '<p>An samhradh seo caite, bhí mé ar mo laethanta saoire. '
         + 'Bhí an grian ag scoilteadh na gcloch. Bhí mé cois farraige. Go tobann, '
         + 'thosaigh mé ag troid le mo chara agus thosaigh mo shrón ag cur fola. '
-        + 'Chuir an bhean glaoch ar mo thuismitheoirí. Ní dhéanfaidh mé dearmad go deo ar an lá sin. </p><br><br><br><br>';
+        + 'Chuir an bhean glaoch ar mo thuismitheoirí. Ní dhéanfaidh mé dearmad go deo ar an lá sin. </p><br><br><br>';
     var heading = '<h1>The Accident</h1><br>';
     var story = '<p>Last summer, I was on holidays. '
         + 'The sun was splitting the stones. I was at the seaside. Suddenly, '
         + 'I started fighting with my friend and my nose started bleeding. '
-        + 'The woman called my parents. I will never forget that day. </p><br><br><br><br>';
-    return ceannteideal + scéal + heading + story;
+        + 'The woman called my parents. I will never forget that day. </p><br><br><br>';
+    document.getElementById('timpiste').innerHTML = ceannteideal + scéal + heading + story;
+}
+function createButton() {
+    return '<button type="button" class="btn btn-success btn-lg" onClick="scéalAScríobh()">Timpiste Nua</button>'
+        + '<br><br><br>'
+        + '<div id="timpiste"></div>';
 }
 // update the HTML here
-function taispeáinAnScéal() {
-    return scéalAScríobh();
+function taispeáinGachRud() {
+    var gachRud = createButton();
+    return gachRud;
 }
-document.querySelector('main').innerHTML = taispeáinAnScéal();
+document.querySelector('main').innerHTML = taispeáinGachRud();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,28 @@ function scéalAScríobh() {
   let scéal = '<p>An samhradh seo caite, bhí mé ar mo laethanta saoire. '
   + 'Bhí an grian ag scoilteadh na gcloch. Bhí mé cois farraige. Go tobann, '
   + 'thosaigh mé ag troid le mo chara agus thosaigh mo shrón ag cur fola. '
-  + 'Chuir an bhean glaoch ar mo thuismitheoirí. Ní dhéanfaidh mé dearmad go deo ar an lá sin. </p><br><br><br><br>';
+  + 'Chuir an bhean glaoch ar mo thuismitheoirí. Ní dhéanfaidh mé dearmad go deo ar an lá sin. </p><br><br><br>';
 
   let heading = '<h1>The Accident</h1><br>'
   let story = '<p>Last summer, I was on holidays. '
   + 'The sun was splitting the stones. I was at the seaside. Suddenly, '
   + 'I started fighting with my friend and my nose started bleeding. '
-  + 'The woman called my parents. I will never forget that day. </p><br><br><br><br>';
+  + 'The woman called my parents. I will never forget that day. </p><br><br><br>';
 
 
-  return ceannteideal + scéal + heading + story;
+  document.getElementById('timpiste').innerHTML = ceannteideal + scéal + heading + story;
+}
+
+function createButton() {
+  return '<button type="button" class="btn btn-success btn-lg" onClick="scéalAScríobh()">Timpiste Nua</button>'
+  + '<br><br><br>'
+  + '<div id="timpiste"></div>';
 }
 
 // update the HTML here
-function taispeáinAnScéal() {
-  return scéalAScríobh();
+function taispeáinGachRud() {
+  let gachRud = createButton();
+  return gachRud;
 }
 
-document.querySelector('main').innerHTML = taispeáinAnScéal();
+document.querySelector('main').innerHTML = taispeáinGachRud();
